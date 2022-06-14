34a3eef31d915b4e2301e4fc048473e5da86fde5
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-14 12:16:26 -0400

    finished problem

--- leet_code.js
+++ leet_code.js
@@ -64,19 +64,23 @@
 // Day 3 Problem:
 // make a function that takes in roman numerals and returns a number
 
-const numeralToNumber = function (input) {
-    input = input.toUpperCase()
-    let numerals = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
-    let output = 0
-    for (let i = 0; i < input.length; i++) {
-        console.log(input[i])
-        if (numerals[input[i + 1]] > numerals[input[i]]) {
-            output += numerals[input[i + 1]] - numerals[input[i]]
-            i++
-        } else {
-            output += numerals[input[i]]
-        }
-    }
-    return output
-};
-console.log(numeralToNumber("MCMXCIV"))
+// const numeralToNumber = function (input) {
+//     input = input.toUpperCase()
+//     let numerals = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+//     let output = 0
+//     for (let i = 0; i < input.length; i++) {
+//         console.log(input[i])
+//         if (numerals[input[i + 1]] > numerals[input[i]]) {
+//             output += numerals[input[i + 1]] - numerals[input[i]]
+//             i++
+//         } else {
+//             output += numerals[input[i]]
+//         }
+//     }
+//     return output
+// };
+// console.log(numeralToNumber("ix"))
+
+//   ------------------------------------------------
+// Day 4 Problem:
+// 
