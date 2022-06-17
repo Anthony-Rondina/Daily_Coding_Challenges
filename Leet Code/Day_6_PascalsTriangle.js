ef0d8b36aa1c9d8a36372eff1daeb21e1857a874
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-17 16:22:58 -0400

    puzzled it out

--- Leet Code/Day_6_PascalsTriangle.js
+++ Leet Code/Day_6_PascalsTriangle.js
@@ -3,8 +3,17 @@
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 var generate = function (numRows) {
-    let output = []
-    let
+    let output = [[1]];
+    for (let i = 0; i < numRows - 1; i++) {
+        let check = output[output.length - 1];
+        let row = [1];
+        for (let j = 1; j < check.length; j++) {
+            row.push(check[j] + check[j - 1])
+        }
+        row.push(1);
+        output.push(row);
+    }
+    return output;
 };
 
-generate(5)
+console.log(generate(5))
