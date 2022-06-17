58134b5b6789779dfee3902a05736f2dcac9a138
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-17 11:15:47 -0400

    first commit

---
+++ Leet Code/Day_6_PascalsTriangle.js
@@ -0,0 +1,10 @@
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+var generate = function (numRows) {
+    let output = []
+    let
+};
+
+generate(5)
