d4603edd96bd9b0428046a96d4b91cf592845204
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-24 19:06:30 -0400

    solved with frequency counter

--- Leet Code/Day11_SingleNumber.js
+++ Leet Code/Day11_SingleNumber.js
@@ -3,16 +3,16 @@
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 const singleNumber = (nums) => {
-    let result = 0
-    nums.sort()
+    let freq = {}
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (nums[i] !== nums[j] && i != j) {
-                result = nums[i]
-            }
+        let character = nums[i];
+        if (freq[character]) {
+            freq[character]++;
+        } else {
+            freq[character] = 1;
         }
     }
-    return result
+    return Object.keys(freq).find(key => freq[key] === 1)
 }
 
-console.log(singleNumber([4, 1, 2, 1, 2]))
+console.log(singleNumber([10, 10, 20, 20, 2, 2, 1, 1, 2, 3, 2, 3, 5, 5, 6]))
