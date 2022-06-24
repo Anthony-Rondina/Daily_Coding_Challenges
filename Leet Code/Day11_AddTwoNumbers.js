3316e988ab2e6e8535fc393844ff8a6c4ebe3a4f
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-24 17:41:14 -0400

    final solve with comments

--- Leet Code/Day11_AddTwoNumbers.js
+++ Leet Code/Day11_AddTwoNumbers.js
@@ -22,4 +22,4 @@ const removeDuplicates = (nums) => {
     return { k, nums }
 };
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+console.log(removeDuplicates([0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 9, 9, 9, 9]))
