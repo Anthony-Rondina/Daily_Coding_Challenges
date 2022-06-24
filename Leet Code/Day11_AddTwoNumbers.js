d6a87bf905bdd6c57f2a7979dca3811cd8f66a6a
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-24 16:59:02 -0400

    started problem

--- Leet Code/Day11_AddTwoNumbers.js
+++ Leet Code/Day11_AddTwoNumbers.js
@@ -1,3 +1,11 @@
-// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
-// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+const removeDuplicates = (nums) => {
+
+};
