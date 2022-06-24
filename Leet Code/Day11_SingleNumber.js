3e30edbb1a2e364337b55b29ddf9125d2a0a66cf
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-24 19:13:08 -0400

    added comments to solution answer

--- Leet Code/Day11_SingleNumber.js
+++ Leet Code/Day11_SingleNumber.js
@@ -3,15 +3,22 @@
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 const singleNumber = (nums) => {
+    //starting a frequency counter object
     let freq = {}
+    //Loop through array
     for (let i = 0; i < nums.length; i++) {
+        //set my focused characters as the current item in array
         let character = nums[i];
+        //If the item currently exists in my frequency counter object then increase it's value by 1
         if (freq[character]) {
             freq[character]++;
         } else {
+            //If it does not exist in the frequency counter object then initialize it with a value of 1
             freq[character] = 1;
         }
     }
+
+    //look through the keys in the frequency counter object and return the key who's value is 1
     return Object.keys(freq).find(key => freq[key] === 1)
 }
 
