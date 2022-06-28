3971159b7f8d64242c3e06c19f812ff50b1a4f31
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-27 22:08:41 -0400

    updated code

--- Leet Code/leet_Code.js
+++ Leet Code/leet_Code.js
@@ -4,23 +4,23 @@
 // const maximumWealth = (arr) => {
 //     let total = 0;
 
-//     // arr we looking at all accounts in the array 
-//     //accounts is looking at indvidual bank account 
-//     arr.forEach((accounts) => {
-//         // let subtotal is being scoped so we can have each indvidual account to casue sperations.
-//         let subTotal = 0;
-//         //numbers are each number in the arrary for each indvidual person 
-//         accounts.forEach((number) => {
-//             // console.log("number is", number);
-//             subTotal += number
-//             // console.log("subtotal is", subTotal);
-//             if (subTotal >= total) {
-//                 total = subTotal
-//             }
-//         })
-//     })
-//     return total
-// };
+// arr we looking at all accounts in the array 
+//accounts is looking at indvidual bank account 
+arr.forEach((accounts) => {
+    // let subtotal is being scoped so we can have each indvidual account to casue sperations.
+    let subTotal = 0;
+    //numbers are each number in the arrary for each indvidual person 
+    accounts.forEach((number) => {
+        // console.log("number is", number);
+        subTotal += number
+        // console.log("subtotal is", subTotal);
+        if (subTotal >= total) {
+            total = subTotal
+        }
+    })
+})
+return total
+};
 
 
 
