cf6684021630b17eb0e128300077e97da62a5b60
Anthony-Rondina <anthony.rondina@icloud.com>
2022-06-29 13:18:05 -0400

    started problem

---
+++ Leet Code/Day17_#1528_ShuffleString.js
@@ -0,0 +1,12 @@
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+const restoreString = (s, indicies) => {
+    for (let i = 0; i < s.length; i++) {
+
+    }
+
+}
+
+console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]))
