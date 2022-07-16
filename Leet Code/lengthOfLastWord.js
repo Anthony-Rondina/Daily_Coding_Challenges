7e1adf2c6c1646e49834520ba34493f5c4c1d25d
Anthony-Rondina <anthony.rondina@icloud.com>
2022-07-16 12:53:27 -0400

    finished another problem

--- Leet Code/lengthOfLastWord.js
+++ Leet Code/lengthOfLastWord.js
@@ -1,6 +1,22 @@
-var lengthOfLastWord = function (s) {
-    let arr = s.trim().split(' ')
-    return arr[arr.length - 1].length
+// var lengthOfLastWord = function (s) {
+//     let arr = s.trim().split(' ')
+//     return arr[arr.length - 1].length
+// };
+
+// console.log(lengthOfLastWord("   fly me   to   the moon  "))
+
+var isPalindrome = function (s) {
+    str = s.replace(/[^\w\s]|_/g, "")
+        .replace(/\s+/g, "");
+    let check = str.toLowerCase()
+    let reverse = str.toLowerCase().split('').reverse().join('')
+    console.log(check, reverse)
+    if (check == reverse) {
+        return true
+    } else {
+        return false
+    }
 };
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(isPalindrome(
+    "Marge, let's \"[went].\" I await {news} telegram."))
