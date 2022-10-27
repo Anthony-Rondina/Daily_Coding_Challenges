fd3e84692c5f60ce11fc2c4a5aeb3fb235d6acd8
Anthony-Rondina <anthony.rondina@icloud.com>
2022-10-27 19:00:21 -0400

    updating code

--- Leet Code/largest3SameDigit.js
+++ Leet Code/largest3SameDigit.js
@@ -1,23 +1,18 @@
 const largestGoodInteger = (num) => {
     let output = ""
     let currentLargest = ""
-    let numArr = num.split('')
-    console.log(numArr)
-    for (let i = 0; i < numArr.length - 2; i++) {
-        console.log(numArr[i], numArr[i + 1], numArr[i + 2])
-        if (numArr[i] === numArr[i + 1] && numArr[i] === numArr[i + 2]) {
-            console.log('success')
-            currentLargest = numArr[i] + numArr[i + 1] + numArr[i + 2]
-            console.log('current is', currentLargest)
+    // let numArr = num.split('')
+    for (let i = 0; i < num.length - 2; i++) {
+        if (num[i] === num[i + 1] && num[i] === num[i + 2]) {
+            // currentLargest = num[i] + num[i + 1] + num[i + 2]
+            currentLargest.push(num[i], num[i + 1], num[i + 2])
             if (currentLargest >= output) {
                 output = currentLargest.toString()
             }
         }
     }
-    if (output.length > 3) {
-        output = ''
-    }
+
     return output
 };
 
-console.log(largestGoodInteger("42352338"))
+console.log(largestGoodInteger("4233352338"))
