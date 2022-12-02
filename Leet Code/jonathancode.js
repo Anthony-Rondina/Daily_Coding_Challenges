b8e08cca0730a40bafb92b9c766657330eb5582e
Anthony-Rondina <anthony.rondina@icloud.com>
2022-12-02 18:13:30 -0500

    completed a lesson with Jon

--- Leet Code/jonathancode.js
+++ Leet Code/jonathancode.js
@@ -6,19 +6,35 @@
 
 
 
-const dayOfTheWeek = (day, month, year) => {
-    let a = new Date(year, month - 1, day).toString().substring(0, 3);
-    let b = {
-        Sun: "Sunday",
-        Mon: "Monday",
-        Tue: "Tuesday",
-        Wed: "Wednesday",
-        Thu: "Thursday",
-        Fri: "Friday",
-        Sat: "Saturday",
-    };
+// const dayOfTheWeek = (day, month, year) => {
+//     let a = new Date(year, month - 1, day).toString().substring(0, 3);
+//     let b = {
+//         Sun: "Sunday",
+//         Mon: "Monday",
+//         Tue: "Tuesday",
+//         Wed: "Wednesday",
+//         Thu: "Thursday",
+//         Fri: "Friday",
+//         Sat: "Saturday",
+//     };
 
-    return b[a];
+//     return b[a];
+// }
+
+// console.log(dayOfTheWeek(1, 10, 2020))
+
+const convertCase = (input) => {
+    arr = input.split('')
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] == "-" || arr[i] == "_") {
+            arr.splice(i, 1)
+            let next = arr[i]
+            arr.splice(i, 1, next.toUpperCase())
+        }
+    }
+    console.log(arr.join(''))
 }
 
-console.log(dayOfTheWeek(1, 10, 2020))
+
+convertCase('the-stealth_warrior')
